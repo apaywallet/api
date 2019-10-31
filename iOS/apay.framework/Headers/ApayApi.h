6698005d3e6b446b41cc91f08ee264659a93ba91
zxg <15635395802@163.com>
2019-10-31 11:41:26 +0800

    修改一些东西

--- iOS/apay.framework/Headers/ApayApi.h
+++ iOS/apay.framework/Headers/ApayApi.h
@@ -13,13 +13,11 @@
 @protocol ApayApiDelegate <NSObject>
 @optional
 
-/*! @brief 发送一个sendReq后，收到Apay的回应
+/*! @brief 发送一个sendApayReq后，收到Apay的回应
  *
- * 收到一个来自Apay的处理结果。调用一次sendReq后会收到onResp。
- * 可能收到的处理结果有SendMessageToWXResp、SendAuthResp等。
- * @param resp具体的回应内容，是自动释放的
+ * 收到一个来自Apay的处理结果。调用一次sendApayReq后会收到onApayResp。
  */
-- (void)onResp:(ApayBaseResp*)resp;
+- (void)onApayResp:(ApayBaseResp*_Nullable)resp;
 
 @end
 
@@ -37,12 +35,11 @@
 
 /*! @brief 发送请求到Apay，等待Apay返回onResp
  *
- * 函数调用后，会切换到Apay的界面。第三方应用程序等待Apay返回onResp。Apay在异步处理完成后一定会调用onResp。支持以下类型
- * SendAuthReq、SendMessageToWXReq、PayReq等。
+ * 函数调用后，会切换到Apay的界面。第三方应用程序等待Apay返回onResp。Apay在异步处理完成后一定会调用onApayResp。
  * @param req 具体的发送请求，在调用函数后，请自己释放。
  * @return 成功返回YES，失败返回NO。
  */
-+ (BOOL)sendReq:(ApayBaseReq*)req;
++ (BOOL)sendApayReq:(ApayBaseReq*_Nullable)req;
 
 /*! @brief 检查Apay是否已被用户安装,前提是添加APAY的urlsechme到白名单
  *
@@ -51,7 +48,7 @@
 + (BOOL)isAPAYAppInstalled;
 
 /// 首先必须设置appid
-+ (BOOL)registerApp:(NSString *)appid;
++ (BOOL)registerApayApp:(NSString *_Nullable)appid;
 
 /// 是否打印日志
 + (void)openLogs:(BOOL)isOpenLog;
